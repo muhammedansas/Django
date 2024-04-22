29e2a5918bda61fa77745812870d2b12ce6471d1
Muhammed Ansas pt <muhammedansas566@gmail.com>
2024-04-22 14:11:36 +0530

    updated

--- cinima/views.py
+++ cinima/views.py
@@ -38,25 +38,14 @@ def delete(request,id):
 
 def update(request,id):
     movie = Movies.objects.get(id=id)
-    fmovie = Fmovie(instance=movie)
-    if request.POST:
-        fmovie = Fmovie(request.POST)
-        if fmovie.is_valid():
-            fmovie.save()
-    return render(request,'m_update.html',{"movie":movie,"fmovie":fmovie})
+    fmovie = Fmovie(request.POST,instance=movie)
+    if fmovie.is_valid():
+        fmovie.save()
+        return redirect('m_home')
+    fmovie =Fmovie(instance=movie)
+    return render(request,'m_update.html',{"fmovie":fmovie})
 
-def update(request,id):
-    movie_name = request.POST["movie_name"]
-    movie_type = request.POST['movie_type']
-    description = request.POST["description"]
-    movie_image = request.POST["movie_image"]
-    movie = Movies.objects.get(id = id)
-    movie.movie_name = movie_name
-    movie.movie_type = movie_type
-    movie.description = description
-    movie.movie_image = movie_image
-    movie.save()
-    return redirect('/m_home')
+
 
 
    
